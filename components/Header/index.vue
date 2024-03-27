8688c1f56cff1e96b9223ee0e41a65b94dc31c06
Yaroslav <kuzivyaroslav1999@gmail.com>
2024-03-28 00:08:50 +0100

    [Web]: Added logo and header

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <div class="underline">Header</div>
+  <div>
+    <header class="flex justify-between p-5 pt-2 mb-5">
+      <NuxtLink to="/" class="w-[100px]">
+        <img src="../../assets/img/logo.png" alt="logo" />
+      </NuxtLink>
+      <HeaderLinks />
+      <div>
+        <button class="bg-[#eee7da] text-[#afc8ad] py-2 px-4 rounded-full">
+          Zalogować się
+        </button>
+      </div>
+    </header>
+    <section>
+      <div class="custom-shape-divider-top-1711316342 z-[-1]">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </section>
+  </div>
 </template>
